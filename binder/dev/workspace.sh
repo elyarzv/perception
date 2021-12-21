6d26bec90cb545bdcfa75de8b95189b7ad00e463
behnammoradi <b.moradi@ai-systems.ca>
2021-12-21 15:47:54 -0800

    preparing the base image

---
+++ binder/dev/workspace.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export PROJECT=phoenix1-perception
+export WORKSPACE=${HOME}
+export ROBOT_WS=${WORKSPACE}/robot_ws
+export LOG_DIR=${HOME}'/.ais/logs/'
+export ROS_DISTRO=noetic
+export TERM=xterm-256color
+export PATH="/root/.local/bin:$PATH"
+export PATH="${WORKSPACE}/field-scripts:$PATH"
